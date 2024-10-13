84d513e980564c9fde86f322fd60e3547977d6f2
TramNBNguyen <177782264+TramNBNguyen@users.noreply.github.com>
2024-10-12 21:19:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// class App extends React.Component {
+//     handleGenderSelection = (gender) => {
+//         alert(`You selected ${gender}`);
+//     }
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <div className="logo">
+//                     Your Best Friend
+//                 </div>
+//                 <div className="figure-container">
+//                     <div className="figure"></div>
+//                     <div className="message">
+//                         For now, we only have a limited choice for the figure, but we're working on a customizable one.
+//                     </div>
+//                     <div className="gender-buttons">
+//                         <button 
+//                             className="gender-button" 
+//                             onClick={() => this.handleGenderSelection('Female')}
+//                         >
+//                             Female
+//                         </button>
+//                         <div className="arrow arrow-left"></div>
+//                         <button 
+//                             className="gender-button" 
+//                             onClick={() => this.handleGenderSelection('Male')}
+//                         >
+//                             Male
+//                         </button>
+//                         <div className="arrow arrow-right"></div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
